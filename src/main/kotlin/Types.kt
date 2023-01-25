6237ece7b8c3e302a48438d409c7627c9264d024
SilverAndro <52360088+silverandro@users.noreply.github.com>
2023-01-25 12:55:58 -0500

    Turns out none of those are mutable

--- src/main/kotlin/Types.kt
+++ src/main/kotlin/Types.kt
@@ -8,56 +8,47 @@ val types = buildList {
     add(MathType(
         "org.joml.Vector2d",
         MathType.Components.XY,
-        MathType.BackingType.DOUBLE,
-        false
+        MathType.BackingType.DOUBLE
     ))
     add(MathType(
         "org.joml.Vector2f",
         MathType.Components.XY,
-        MathType.BackingType.FLOAT,
-        false
+        MathType.BackingType.FLOAT
     ))
     add(MathType(
         "org.joml.Vector2i",
         MathType.Components.XY,
-        MathType.BackingType.INT,
-        false
+        MathType.BackingType.INT
     ))
     add(MathType(
         "org.joml.Vector3f",
         MathType.Components.XYZ,
-        MathType.BackingType.FLOAT,
-        false
+        MathType.BackingType.FLOAT
     ))
     add(MathType(
         "org.joml.Vector3d",
         MathType.Components.XYZ,
-        MathType.BackingType.DOUBLE,
-        false
+        MathType.BackingType.DOUBLE
     ))
     add(MathType(
         "org.joml.Vector3i",
         MathType.Components.XYZ,
-        MathType.BackingType.INT,
-        false
+        MathType.BackingType.INT
     ))
     add(MathType(
         "org.joml.Vector4f",
         MathType.Components.XYZW,
-        MathType.BackingType.FLOAT,
-        false
+        MathType.BackingType.FLOAT
     ))
     add(MathType(
         "org.joml.Vector4d",
         MathType.Components.XYZW,
-        MathType.BackingType.DOUBLE,
-        false
+        MathType.BackingType.DOUBLE
     ))
     add(MathType(
         "org.joml.Vector4i",
         MathType.Components.XYZW,
-        MathType.BackingType.INT,
-        false
+        MathType.BackingType.INT
     ))
 
     //
@@ -67,32 +58,27 @@ val types = buildList {
     add(MathType(
         "net.minecraft.util.math.Vec2f",
         MathType.Components.XY,
-        MathType.BackingType.FLOAT,
-        false
+        MathType.BackingType.FLOAT
     ))
     add(MathType(
         "net.minecraft.util.math.Vec3d",
         MathType.Components.XYZ,
-        MathType.BackingType.DOUBLE,
-        false
+        MathType.BackingType.DOUBLE
     ))
     add(MathType(
         "net.minecraft.util.math.Vec3i",
         MathType.Components.XYZ,
-        MathType.BackingType.INT,
-        false
+        MathType.BackingType.INT
     ))
     add(MathType(
         "net.minecraft.util.math.BlockPos",
         MathType.Components.XYZ,
-        MathType.BackingType.INT,
-        false
+        MathType.BackingType.INT
     ))
     add(MathType(
         "net.minecraft.client.util.math.Vector2f",
         MathType.Components.XY,
-        MathType.BackingType.FLOAT,
-        false
+        MathType.BackingType.FLOAT
     ))
 }
 
--- src/main/kotlin/data/MathType.kt
+++ src/main/kotlin/data/MathType.kt
@@ -1,6 +1,6 @@
 package data
 
-data class MathType(val path: String, val components: Components, val backingType: BackingType, val isMutable: Boolean) {
+data class MathType(val path: String, val components: Components, val backingType: BackingType) {
     val name = path.split(".").last()
     val uniqueName = path.split(".").dropLast(1).map { it.first().uppercase() }.joinToString(separator = "", prefix = "", postfix = "$") + name
     override fun toString() = name
